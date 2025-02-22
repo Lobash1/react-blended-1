ef3dd8c483ce9904f8bb399c7229cdd125860549
Anastasiia <lobash.n@gmail.com>
2025-02-22 21:28:21 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 import article from './data/article.json';
 import stats from './data/stats.json';
+import forbes from './data/forbes.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
@@ -26,9 +28,10 @@ export const App = () => {
         <Statistics title="Main Statistics" stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
+
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
